63999f4d474b4f9a1405f883794124db3befd077
Johan Segerfeldt <johan.segerfeldt@me.com>
2015-09-26 07:30:21 +0200

    Add first test

---
+++ tests/ToRomanTests.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ToRomanTests {
+
+	@Test
+	public void romanHasNoZero() {
+		assertEquals("", roman(0));
+	}
+
+	private String roman(int value) {
+		return "";
+	}
+
+}
