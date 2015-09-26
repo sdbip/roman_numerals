4e573bc74466a7d53c6e7c1e702ad48ad1da83e4
Johan Segerfeldt <johan.segerfeldt@me.com>
2015-09-26 07:33:18 +0200

    Add test for Ⅴ = 5

--- tests/ToRomanTests.java
+++ tests/ToRomanTests.java
@@ -14,9 +14,15 @@ public class ToRomanTests {
 		assertEquals("I", roman(1));
 	}
 
+	@Test
+	public void romanForFive() {
+		assertEquals("V", roman(5));
+	}
+
 
 	private String roman(int value) {
 		if (value == 1) return "I";
+		if (value == 5) return "V";
 		return "";
 	}
 
