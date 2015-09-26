35b503d0c1554f60fc8d893ce239e6b957adacc6
Johan Segerfeldt <johan.segerfeldt@me.com>
2015-09-26 08:30:52 +0200

    First test for converting roman back

---
+++ tests/FromRomanTests.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class FromRomanTests {
+
+	@Test
+	public void fromEmptyRomanStringReturnsZero() {
+		assertEquals(0, fromRoman(""));
+	}
+
+	private int fromRoman(String roman) {
+		return 0;
+	}
+
+}
